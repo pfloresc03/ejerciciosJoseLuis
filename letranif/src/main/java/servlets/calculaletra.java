d0f33bd053443e62b07e1747723aa189639afeea
Pablo <pabloflores11@hotmail.com>
2020-12-14 21:26:23 +0100

    version 1

---
+++ letranif/src/main/java/servlets/calculaletra.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pablo Flores
+ */
+public class calculaletra extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'};
+            String dni = request.getParameter("dni");
+            String letra = request.getParameter("letra");
+            int dniEntero = Integer.parseInt(dni);
+            int indice = dniEntero%23;
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet calculaletra</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet calculaletra at " + request.getContextPath() + "</h1>");
+            out.println("<h2>DNI: "+ dni + "</h2>");
+            out.println("<h2>Letra: "+ letra +"</h2>");
+            out.println("<h2>Letra calculada: "+ letras[indice] +"</h2>");
+            if (letra.equalsIgnoreCase(Character.toString(letras[indice]))){
+                out.println("<p>Has introducido correctamente la letra</p>");
+            } else {
+                out.println("<p>La letra que has introducido no es correcta</p>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ letranif/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>LetraNIF</title>
+    </head>
+    <body>
+        <h1>Calcula Letra NIF</h1>
+        <form action="calculaletra">
+            <p><label>DNI: <input type="text" name="dni"></label></p>
+            <p><label>Letra: <input type="text" name="letra"></label></p>
+            <p><input type="submit" value="Comprobar letra" ></p>
+        </form>
+    </body>
+</html>
